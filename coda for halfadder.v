7c560e6e42f209bbbae60a934f9455afdd588e81
Jeya Sree Krishnamoorthy <115264091+JeyasreeRK@users.noreply.github.com>
2023-07-26 13:27:13 +0530

    halfadder.v

--- coda for halfadder.v
+++ coda for halfadder.v
@@ -1,4 +1,4 @@
-coda for halfadder
+ halfadder
 
 module halfadder(
     input a,b,
@@ -11,4 +11,42 @@ module halfadder(
 
 
 endmodule
+
+test bench
+ module halfadd;
+
+	// Inputs
+	reg a;
+	reg b;
+
+	// Outputs
+	wire sum;
+	wire co;
+
+	// Instantiate the Unit Under Test (UUT)
+	halfadder uut (
+		.a(a), 
+		.b(b), 
+		.sum(sum), 
+		.co(co)
+	);
+
+	initial begin
+		// Initialize Inputs
+		a = 1'b0;
+		b = 1'b0;
+
+		// Wait 100 ns for global reset to finish
+		#100;a=1'b0;b=1'b1;
+		#100;a=1'b1;b=1'b0;
+		#100;a=1'b1;b=1'b1;
+		#100$finish;
+        
+		// Add stimulus here
+
+	end
+      
+endmodule
+
+
 
